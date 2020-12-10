abf7630678c38769da38c4199a29367311915c0c
lgrignon <louis.grignon@gmail.com>
2020-12-10 16:37:19 +0100

    deploy fixes

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
 mvn clean && mvn -P client generate-sources && mvn package
 
-nohup sh -c "$JAVA_HOME/bin/java -jar target/jsweet-live-sandbox-0.0.1-SNAPSHOT.jar" &
+nohup sh -c "$JAVA_HOME/bin/java -jar target/jsweet-live-sandbox-3.0.0-jar-with-dependencies.jar" &
